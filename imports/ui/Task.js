de9e45d121767b0a717589f214908c30f8b135db
whekin <stanislavkalishin@gmail.com>
2018-11-25 23:41:51 +0300

    Update components to use new sequre  meteor methods

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
 import { Tasks } from '../api/tasks';
 
 // Task component - represents a single todo item
 export default class Task extends Component {
   toggleChecked = () => {
-    Tasks.update(this.props.task._id, {
-      $set: { checked: !this.props.task.checked }
-    });
+    Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked);
   }
 
   deleteThisTask = () => {
-    Tasks.remove(this.props.task._id);
+   Meteor.call('tasks.remove', this.props.task._id);
   }
   
   render() {
